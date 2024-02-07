f4c04994886b874c55094cedbf881a689c255b5c
Chris Lail <chrislail@protonmail.com>
2024-02-07 16:53:10 -0500

    add random transition, angles, and positions for wallpaper changes

--- hyprland/.config/hypr/scripts/refresh-theme.sh
+++ hyprland/.config/hypr/scripts/refresh-theme.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-# Define the directory where wallpapers are stored
+# Function to get a random element from an array
+random_element() {
+  local array=("$@")
+  local index=$(($RANDOM % ${#array[@]}))
+  echo "${array[$index]}"
+}
+
+# Directory where wallpapers are stored
 wallpaper_dir="$HOME/.config/hypr/wallpapers"
 
-# Get a random wallpaper file from the directory
+# Select a random wallpaper file from the directory
 wallpaper="${wallpaper_dir}/$(ls -1 "$wallpaper_dir" | shuf -n1)"
 
+# Transition and position options
+transitions=("wipe" "grow" "center" "outer" "fade")
+positions=("center" "top" "left" "right" "bottom" "top-left" "top-right" "bottom-left" "bottom-right")
+
+# Get a random transition, position, and angle
+selected_transition=$(random_element "${transitions[@]}")
+selected_position=$(random_element "${positions[@]}")
+selected_angle=$((RANDOM % 360))
+
 # Set wallpaper via swww
-swww img $wallpaper
+swww img "$wallpaper" --transition-fps  165 --transition-type "$selected_transition" --transition-pos "$selected_position" --transition-angle "$selected_angle"
 
 # Set general theme colors via pywal
-wal -q -i $wallpaper
+wal -q -i "$wallpaper"
 
-# Set firefox theme colors via pywal
+# Set Firefox theme colors via pywal
 pywalfox update
