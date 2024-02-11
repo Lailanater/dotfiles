cb51ff6831042d4d6d2bd67399a6e90a58851a12
Chris Lail <chrislail@protonmail.com>
2024-02-10 21:00:27 -0500

    add comments and safety to stow.sh

--- stow.sh
+++ stow.sh
@@ -1,2 +1,8 @@
+# Stow all directories in this repo
 stow */
+
+# Ensure that the steam folder is created
+mkdir -p ~/.steam/steam
+# Cannot stow steam_dev.cfg so the work around is to just copy
+# This fixes slow steam download speed on linux
 cp ./steam/steam_dev.cfg ~/.steam/steam
