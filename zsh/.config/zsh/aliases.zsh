e35569f18ec0a17f352bc1217f8292f7aa02d130
Chris Lail <chrislail@protonmail.com>
2024-03-18 09:34:32 -0400

    Add flatpak update to update command and use gecko for neofetch on openSUSE

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,10 @@
-alias update="sudo zypper dup"
+alias update="sudo zypper dup && printf '\nUpdating Flatpak...\n------------------------------\n' && flatpak update"
+
 alias ls="eza --long"
+
 alias reboot="systemctl reboot"
 alias restart="reboot"
 alias poweroff="systemctl poweroff"
 alias shutdown="poweroff"
+
+alias neofetch="neofetch --ascii_distro openSUSE"
