0ca3d53db6ada7fa07c9f25b37e72b5fa6670a3d
Chris Lail <chrislail@protonmail.com>
2024-10-18 13:43:49 -0700

    Update hyprland monitor config
    Change to new Samsung Odyssey G6 OLED

--- hyprland/.config/hypr/conf/monitor.sh
+++ hyprland/.config/hypr/conf/monitor.sh
@@ -1,15 +1,15 @@
 # See https://wiki.hyprland.org/Configuring/Monitors/
-monitor=DP-2,2560x1440@165,0x1440,1
+monitor=DP-1,2560x1440@360,0x1440,1
 monitor=HDMI-A-2,2560x1440@70,0x0,1
 
 # Default monitor behavior
 monitor=,preferred,auto,auto
 
-workspace=1,monitor:DP-2
-workspace=2,monitor:DP-2
-workspace=3,monitor:DP-2
-workspace=4,monitor:DP-2
-workspace=5,monitor:DP-2
+workspace=2,monitor:DP-1
+workspace=3,monitor:DP-1
+workspace=4,monitor:DP-1
+workspace=1,monitor:DP-1
+workspace=5,monitor:DP-1
 workspace=6,monitor:HDMI-A-2
 workspace=7,monitor:HDMI-A-2
 workspace=8,monitor:HDMI-A-2
