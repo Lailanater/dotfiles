2640b80fc8d10e744336a79f44eb92cacc773e5b
Chris Lail <chrislail@protonmail.com>
2024-11-04 13:35:29 -0800

    Add support for Arch Linux plugins
    Refactor logic to be more robust in detecting MacOS and Steam Deck
    homebrew usage vs Linux

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -11,14 +11,15 @@ try_source() {
   fi
 }
 
-# Using Homebrew
-if command_exists brew; then
-  try_source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-  try_source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# Using Zypper (OpenSUSE Tumbleweed)
+if command_exists brew && ("$(uname)" != "Linux" || is_steam_deck); then
+  brew_prefix=$(brew --prefix)
+  try_source $brew_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+  try_source $brew_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 elif command_exists zypper; then
   try_source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
   try_source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif command_exists pacman && ! is_steam_deck; then
+  try_source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+  try_source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
 
