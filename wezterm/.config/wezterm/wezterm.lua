0d28829f095797eee8b2627239eab1c835789232
Chris Lail <chrislail@protonmail.com>
2025-08-01 22:45:57 -0700

    Convert to Monaspace from Jetbrains Mono

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,9 +9,9 @@ return {
 	window_background_opacity = 0.95,
 
 	-- Font settings
-	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
+	font = wezterm.font("Monaspace Neon"),
 	font_size = 14,
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	harfbuzz_features = { "calt" },
 
 	color_scheme = "Catppuccin Mocha",
 
