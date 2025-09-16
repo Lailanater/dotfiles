25c08f6b7b97a3bc20ec7e9770439f9271688131
Chris Lail <chrislail@protonmail.com>
2025-09-16 08:49:39 -0700

    Add colorscheme temp fix

--- home/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ home/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,5 +4,15 @@ return {
     opts = {
       colorscheme = "catppuccin",
     },
-  }
+  },
+  -- temp fix for catppuccin breaking change
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
